90f2918f9ce44352bb7e727e0250cc85907166d5
usatai <usatai.soccer0907@gmail.com>
2025-07-13 23:28:31 +0900

    収支登録機能追加

--- src/main/java/com/example/money/model/Money.java
+++ src/main/java/com/example/money/model/Money.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.example.money.enums.IncomeExpenditureType;
+
 
 import java.util.Date;
 
@@ -24,6 +26,10 @@ public class Money {
     @Column(name="money_price")
     private int money_price;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name="incomeExpenditure_type")
+    private IncomeExpenditureType incomeExpenditureType;
+
     @Column(name="user_id")
     private int user_id;
 
@@ -32,6 +38,4 @@ public class Money {
 
     @Column(name="create_date")
     private Date create_date;
-
-
 }
