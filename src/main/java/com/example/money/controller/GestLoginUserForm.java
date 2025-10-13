d80fbdbb155904c1f64204cf53c1d05d59193093
usatai <usatai.soccer0907@gmail.com>
2025-10-13 10:21:39 +0900

    [fix] ゲストログインバリデーション修正

--- src/main/java/com/example/money/controller/GestLoginUserForm.java
+++ src/main/java/com/example/money/controller/GestLoginUserForm.java
@@ -1,6 +1,12 @@
 package com.example.money.controller;
 
+import com.example.money.validation.UserNameUnique;
+
+import jakarta.validation.constraints.NotBlank;
+
 public record GestLoginUserForm (
+    @NotBlank(message = "ユーザー名は必須です")
+    @UserNameUnique
     String gestLoginUserName
 ){
 }
